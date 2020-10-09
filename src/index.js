a3a76a4fb4aa70214176be727de0584769bba930
harnblaze <harnblaze@gmail.com>
2020-10-09 21:12:35 +0300

    feat: task morse-decoder done

--- src/index.js
+++ src/index.js
@@ -1,46 +1,79 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
 };
 
 function decode(expr) {
-    // write your solution here
+  expr = expr.split("**********");
+  let string = "";
+  expr.forEach((element, i) => {
+    let word = "";
+    let char = "";
+    element = element.split("");
+    while (element.length > 0) {
+      let charCode = element.splice(0, 10);
+      while (charCode.length > 0) {
+        let symbol = charCode.splice(0, 2);
+        symbol = symbol.join("");
+        switch (symbol) {
+          case "11":
+            symbol = "-";
+            break;
+          case "10":
+            symbol = ".";
+            break;
+          default:
+            symbol = "";
+            break;
+        }
+        char += symbol;
+      }
+      char = MORSE_TABLE[char];
+      word += char;
+      char = "";
+    }
+    string += word;
+    if (i < expr.length - 1) {
+      string += " ";
+    }
+  });
+  return string;
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
